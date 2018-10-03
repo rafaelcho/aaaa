f680f1c62b537aa37109255a469221ad2cbe9487
undergraduate <cse20141538@cspro10.sogang.ac.kr>
2018-10-03 03:32:41 -0700

    hello

---
+++ pintos/src/examples/echo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <syscall.h>
+
+int
+main (int argc, char **argv)
+{
+  int i;
+/*
+  for (i = 0; i < argc; i++)
+    printf ("%s ", argv[i]);
+  printf ("\n");
+*/
+  printf("hello world\n");
+  return EXIT_SUCCESS;
+}
